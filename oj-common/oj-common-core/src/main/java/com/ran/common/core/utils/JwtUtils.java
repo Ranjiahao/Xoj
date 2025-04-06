c5a54bd715bf641f9a8006e108d09cfb8d660726
Ranjiahao <ranjiahao_study@163.com>
2025-04-06 23:23:30 +0800

    modify jwt error

--- oj-common/oj-common-core/src/main/java/com/ran/common/core/utils/JwtUtils.java
+++ oj-common/oj-common-core/src/main/java/com/ran/common/core/utils/JwtUtils.java
@@ -1,8 +1,7 @@
 package com.ran.common.core.utils;
 
-import com.ran.common.core.constants.JwtConstants;
 import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.JwtException;
+import com.ran.common.core.constants.JwtConstants;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 
@@ -19,10 +18,7 @@ public class JwtUtils {
      */
     public static String createToken(Map<String, Object> claims, String secret) {
         SecretKey key = Keys.hmacShaKeyFor(secret.getBytes());
-        return Jwts.builder()
-                .claims(claims)
-                .signWith(key)   // 默认使用HS256
-                .compact();
+        return Jwts.builder().claims(claims).signWith(key).compact();
     }
 
     /**
@@ -32,17 +28,13 @@ public class JwtUtils {
      * @param secret 密钥
      * @return 数据
      */
-    public static Claims parseToken(String token, String secret) {
-        try {
-            SecretKey key = Keys.hmacShaKeyFor(secret.getBytes());
-            return Jwts.parser()
-                    .verifyWith(key)
-                    .build()
-                    .parseSignedClaims(token)
-                    .getPayload();
-        } catch (JwtException e) {
-            throw new RuntimeException("JWT令牌解析失败", e);
-        }
+        public static Claims parseToken(String token, String secret) {
+        SecretKey key = Keys.hmacShaKeyFor(secret.getBytes());
+        return Jwts.parser()
+                .verifyWith(key)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
     }
 
     public static String getUserKey(Claims claims) {
